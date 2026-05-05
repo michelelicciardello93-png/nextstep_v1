b49d6e578d7907fd00699e71d52dbff2fe3faedf
michelelicciardello93-png <michelelicciardello93@gmail.com>
2026-05-05 18:13:20 +0200

    Add DB connection

---
+++ backend/lib/db.php
@@ -0,0 +1,16 @@
+<?php
+
+function db() {
+  static $pdo = null;
+
+  if ($pdo === null) {
+    $pdo = new PDO(
+      "mysql:host=localhost;dbname=nextstep;charset=utf8mb4",
+      "root",
+      "",
+      [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]
+    );
+  }
+
+  return $pdo;
+}
